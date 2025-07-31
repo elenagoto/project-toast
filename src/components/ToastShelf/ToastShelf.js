50e1ead20ef9599a0ef905cf1905e0248b3357e9
Laura Gomez <laura@elenagoto.ch>
2025-07-31 21:47:58 +0200

    feat: Add dissmiss all toasts functionality

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -5,8 +5,24 @@ import Toast from '../Toast'
 import styles from './ToastShelf.module.css'
 
 function ToastShelf() {
-  const { listOfToasts } = React.useContext(ToastContext)
+  const { listOfToasts, dismissToast  } = React.useContext(ToastContext)
   const toasts = listOfToasts.filter((toast) => toast.variant !== 'default')
+
+  React.useEffect(() => {
+   function dismissAllToasts(e) {
+      if (e.key !== 'Escape') return    
+      if (toasts.length === 0) return
+
+          toasts.forEach((toast, index) => {
+            setTimeout(() => dismissToast(toast.id), 200 * index)
+          })
+    }
+
+    window.addEventListener('keydown', dismissAllToasts)
+    return () => {
+      window.removeEventListener('keydown', dismissAllToasts)
+    }
+  }, [toasts])
 
   return (
     <ol className={styles.wrapper}>
